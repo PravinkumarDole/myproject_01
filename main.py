9d679296e887a211a6abc9639d16fff63e74c2cb
PravinkumarDole <pvasudeo.dole@gmail.com>
2024-01-06 11:18:19 +0530

    main.py file updated

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pandas as pd
+from source.entity.config_entity import TrainingPipelineConfig
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    train_pipeline_config_obj = TrainingPipelineConfig()
+
+    print(train_pipeline_config_obj.__dict__)
+
